498aea9d314c5ab81ac300faf5c8709004cc6c40
shiyuw <shiyuw0@163.com>
2019-10-28 17:01:49 +0800

    配置路由

--- src/path/index.js
+++ src/path/index.js
@@ -2,9 +2,9 @@
 
 
 const API_URL = process.env.NODE_ENV === 'development' ? {
-  API_URL: 'http://localhost:8096'
+  API_URL: 'http://111.20.145.20:8006/wgproduct/'
 } : {
-  API_URL: 'http://localhost:8096'
+  API_URL: 'http://111.20.145.20:8006/wgproduct/'
 }
 
 export default API_URL
