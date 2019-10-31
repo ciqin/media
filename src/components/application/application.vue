9d33fb197a5d0cebff2da5591225e54f8c3fc87c
shiyuw <shiyuw0@163.com>
2019-10-31 12:09:58 +0800

    解决跳转选项bug

--- src/components/application/application.vue
+++ src/components/application/application.vue
@@ -22,8 +22,10 @@ export default {
     data() {
         return {
             msgNum: 5,
+            // p:this.$route.path,
             // dataType:'',
             activeName:'',
+        
             applicationList:[
                 {
                     titile:"PPT",
@@ -60,12 +62,36 @@ export default {
     },
     methods:{
         storeData(title){
-            debugger;
+            // debugger;
             sessionStorage.setItem('activeName',title);
         },
         goBack(){
             this.$router.back(-1)
         },
+    },
+    computed:{
+        url2Name(){
+            let path = this.$route.path;
+            let name;
+            switch(path){
+                case '/ppt':
+                    name = 'PPT';
+                    break;
+                case '/word':
+                    name = '技术白皮书';
+                    break;
+                case '/prodect':
+                    name = '产品册';
+                    break;
+                case '/video':
+                    name = '视频资料';
+                    break;
+                case '/useurl':
+                    name = '地址';
+                    break;
+            }
+            return name;
+        }
     },
     mounted() {
         // let dataStr = localStorage.getItem('showData');
@@ -74,6 +100,7 @@ export default {
         //     let data = JSON.parse(dataStr);
         //     this.data
         // }
+        
         let activeName = sessionStorage.getItem('activeName');
         if(activeName){
             this.activeName = activeName;
@@ -81,6 +108,42 @@ export default {
             this.activeName = this.applicationList[0].titile;
         }
     },
+    watch:{
+        // url2Name(newVal){
+        //     sessionStorage.setItem('activeName',newVal);
+        // }
+        $route(to,from){
+            let path = to.path;
+            console.log(path)
+            let name;
+            switch(path){
+                case '/ppt':
+                    name = 'PPT';
+                    break;
+                case '/word':
+                    name = '技术白皮书';
+                    break;
+                case '/prodect':
+                    name = '产品册';
+                    break;
+                case '/video':
+                    name = '视频资料';
+                    break;
+                case '/useurl':
+                    name = '地址';
+                    break;
+            }
+            sessionStorage.setItem('activeName',name);
+            
+            let activeName = sessionStorage.getItem('activeName');
+            if(activeName){
+                this.activeName = activeName;
+            }else{
+                this.activeName = this.applicationList[0].titile;
+            }
+
+        }
+    }
 };
 </script>
 
