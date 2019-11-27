16188974b3c9b3cdf7409a659ec8e2727f0ab3a7
shiyuw <shiyuw0@163.com>
2019-11-27 10:04:14 +0800

    修改相对地址

--- src/path/index.js
+++ src/path/index.js
@@ -4,7 +4,7 @@
 const API_URL = process.env.NODE_ENV === 'development' ? {
   API_URL: 'http://localhost:8096'
 } : {
-  API_URL: 'http://192.168.16.1/wgproduct/'
+  API_URL: '/wgproduct/'
 }
 
 export default API_URL
